9f8675b617af0cce247e5e235d90501f8e5e3981
ThomasMAhern <33540039+ThomasMAhern@users.noreply.github.com>
2022-02-06 13:10:51 +0100

    Update download_books.py
    betterer

--- download_books.py
+++ download_books.py
@@ -71,6 +71,33 @@ fuck_ads.click()
 
 
 
+books = ["The Mind's Eye",
+    "The Genius in All of Us: Why Everything You've Been Told About Genetics, Talent, and IQ Is Wrong",
+    "Being Wrong: Adventures in the Margin of Error",
+    "The Invisible Gorilla: And Other Ways Our Intuitions Deceive Us",
+    "The Shallows: What the Internet Is Doing to Our Brains",
+    "Phantoms in the Brain: Probing the Mysteries of the Human Mind",
+    "NurtureShock: New Thinking About Children",
+    "Everyday Survival: Why Smart People Do Stupid Things",
+    "Outliers: The Story of Success",
+    "The God Delusion",
+    "Traffic: Why We Drive the Way We Do and What It Says About Us",
+    "The Unthinkable: Who Survives When Disaster Strikes - and Why",
+    "The Tipping Point: How Little Things Can Make a Big Difference",
+    "The Selfish Gene",
+    "Made to Stick: Why Some Ideas Survive and Others Die",
+    "Blink: The Power of Thinking Without Thinking",
+    "The Man Who Mistook His Wife for a Hat and Other Clinical Tales",
+    "Future Crimes",
+    "Blueprint: How DNA makes us who we are",
+    "I Contain Multitudes: The Microbes Within Us and a Grander View of Life",
+    "Invisible Women: Data Bias in a World Designed for Men",
+    "Behave: The Biology of Humans at Our Best and Worst"]
+
+
+
+
+
 
 
 
@@ -92,13 +119,30 @@ fuck_ads.click()
 
 
 def clicked(xpath):
+    '''Click on a xpath element'''
     driver.find_element_by_xpath(xpath).click()
     
 def fill_a_field(xpath, string):
-    driver.find_element_by_xpath(xpath).send_keys(string)
+    '''Send text values into an input field'''
+    driver.find_element_by_xpath(xpath).send_keys(string)# def get_a_book(book_title):
+
+def navigate_to_book_dl(book_name):
+    '''Give book name, it clicks through to result'''
+    fill_a_field(xpath_search_bar, book_name)
+    clicked(xpath_search_button)
+    clicked(xpath_first_result_button)
+
+def actually_download():
+    '''From book page, click and wait for download'''
+    clicked(xpath_download_button)
+    time.sleep(15) # wait for it to tasty the pdf file
+    clicked(xpath_get_pdf_button) # actually click the download
+    time.sleep(6) # wait for it to finish downloading
     
+# Where are the things?
 location_of_chrome_driver = '~/Downloads/chromedriver'
 website_to_visit = 'https://www.pdfdrive.com/'
+# books = ['dataclysm']
 
 # set the xpaths
 xpath_search_bar = '//*[@id="q"]'
@@ -107,19 +151,15 @@ xpath_first_result_button = '/html/body/div[3]/div[1]/div[1]/div[4]/ul/li[1]/div
 xpath_download_button = '//*[@id="download-button-link"]'
 xpath_get_pdf_button = '//*[@id="alternatives"]/div[1]/div/a'
 
-
 # start browser
 driver = webdriver.Chrome(executable_path=location_of_chrome_driver)
 driver.get(website_to_visit)
 
 # run the things
-fill_a_field(xpath_search_bar, 'dataclysm')
-clicked(xpath_search_button)
-clicked(xpath_first_result_button)
-clicked(xpath_download_button)
-time.sleep(15) # wait for it to tasty the pdf file
-clicked(xpath_get_pdf_button) # actually click the download
-time.sleep(6) # wait for it to finish downloading
+for book in books:    
+    navigate_to_book_dl(book)
+    actually_download()
+
 driver.close()
 
 
@@ -158,12 +198,15 @@ driver.close()
 
 
 # and then I tried my hand at playwright
+
 website_to_visit = 'https://www.pdfdrive.com/'
 # books = ['dataclysm', 'oh the places']
 
-xpath_search_bar = '//*[@id="q"]'
+xpath_search_bar = 'id=q'
 xpath_search_button = '//*[@id="search-form"]/button/i'
-xpath_first_result_button = 'h2'
+selector_first_result_button = 'body > div.dialog > div.dialog-main > div.dialog-left > div.files-new > ul > li:nth-child(1) > div > div > div.file-right > a'
+xpath_dropdown_button = '//*[@id="alternatives"]/div[1]/div/button'
+xpath_download_button = '//*[@id="download-button-link"]'
 xpath_get_pdf_button = '//*[@id="alternatives"]/div[1]/div/a'
 xpath_popup = '//*[@id="pdfdriveAlerts"]/div/div/div/i'
 
@@ -181,11 +224,11 @@ async with async_playwright() as p:
     
     
 # Data Extraction Code Here
-    for i in books:
+    for book_title in books:
         try:
-            await page.fill(xpath_search_bar, i) # take xpath and strings from books
+            await page.fill(xpath_search_bar, book_title) # take xpath and strings from books
             await page.click(xpath_search_button);
-            await page.click(xpath_first_result_button);
+            await page.click(selector_first_result_button);
             await page.click(xpath_download_button);
 
             # download tricky bullshit
@@ -197,7 +240,7 @@ async with async_playwright() as p:
             print(download.suggested_filename)
             await download.save_as(f'~/Downloads/{download.suggested_filename}') 
         except:
-            print(f'could not find {i}')
+            print(f'This one failed: {book_title}')
     
     
     
