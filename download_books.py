1130f0cdd3c78ec98209484e028b071b09789e1a
ThomasMAhern <33540039+ThomasMAhern@users.noreply.github.com>
2022-02-06 01:48:32 +0100

    Update download_books.py
    included more schtuff

--- download_books.py
+++ download_books.py
@@ -57,3 +57,151 @@ get_pdf.click()
 # x out of ad
 fuck_ads = driver.find_element_by_xpath('//*[@id="pdfdriveAlerts"]/div/div/div/i')
 fuck_ads.click()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# and then I tried better but less functionality
+
+
+
+def clicked(xpath):
+    driver.find_element_by_xpath(xpath).click()
+    
+def fill_a_field(xpath, string):
+    driver.find_element_by_xpath(xpath).send_keys(string)
+    
+location_of_chrome_driver = '~/Downloads/chromedriver'
+website_to_visit = 'https://www.pdfdrive.com/'
+
+# set the xpaths
+xpath_search_bar = '//*[@id="q"]'
+xpath_search_button = '//*[@id="search-form"]/button/i'
+xpath_first_result_button = '/html/body/div[3]/div[1]/div[1]/div[4]/ul/li[1]/div/div/div[2]/a/h2'
+xpath_download_button = '//*[@id="download-button-link"]'
+xpath_get_pdf_button = '//*[@id="alternatives"]/div[1]/div/a'
+
+
+# start browser
+driver = webdriver.Chrome(executable_path=location_of_chrome_driver)
+driver.get(website_to_visit)
+
+# run the things
+fill_a_field(xpath_search_bar, 'dataclysm')
+clicked(xpath_search_button)
+clicked(xpath_first_result_button)
+clicked(xpath_download_button)
+time.sleep(15) # wait for it to tasty the pdf file
+clicked(xpath_get_pdf_button) # actually click the download
+time.sleep(6) # wait for it to finish downloading
+driver.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# and then I tried my hand at playwright
+website_to_visit = 'https://www.pdfdrive.com/'
+# books = ['dataclysm', 'oh the places']
+
+xpath_search_bar = '//*[@id="q"]'
+xpath_search_button = '//*[@id="search-form"]/button/i'
+xpath_first_result_button = 'h2'
+xpath_get_pdf_button = '//*[@id="alternatives"]/div[1]/div/a'
+xpath_popup = '//*[@id="pdfdriveAlerts"]/div/div/div/i'
+
+
+async with async_playwright() as p: 
+    # start a browser instance
+    browser = await p.webkit.launch()#headless=False)
+    # create a new incognito browser context
+    context = await browser.new_context(accept_downloads= True)    
+    # create a new page inside context.
+    page = await context.new_page()
+    # go to initial page
+    await page.goto(website_to_visit)
+    
+    
+    
+# Data Extraction Code Here
+    for i in books:
+        try:
+            await page.fill(xpath_search_bar, i) # take xpath and strings from books
+            await page.click(xpath_search_button);
+            await page.click(xpath_first_result_button);
+            await page.click(xpath_download_button);
+
+            # download tricky bullshit
+            async with page.expect_download() as download_info:
+                await page.click(xpath_get_pdf_button)
+            download = await download_info.value
+            # waits for download to complete
+            path = await download.path()
+            print(download.suggested_filename)
+            await download.save_as(f'~/Downloads/{download.suggested_filename}') 
+        except:
+            print(f'could not find {i}')
+    
+    
+    
+#     await page.wait_for_timeout(1000)  # Wait for 1 second
+    # dispose context once it is no longer needed.
+    await context.close()
+    await browser.close()
